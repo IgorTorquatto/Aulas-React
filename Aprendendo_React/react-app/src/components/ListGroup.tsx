a490c799d2f96d853dfed02104f1b228c12d1b73
Igor Torquato <igor.torquato.20@gmail.com>
2023-09-20 12:08:57 -0300

    add useSate

--- Aprendendo_React/react-app/src/components/ListGroup.tsx
+++ Aprendendo_React/react-app/src/components/ListGroup.tsx
@@ -1,13 +1,34 @@
+import { MouseEvent, useState } from "react";
+
 function ListGroup() {
+  let items = ["São Paulo", "Rio de Janeiro", "Curitiba"];
+
+  //Capturar um evento de mouse
+  const handleClick = (event: MouseEvent) => console.log(event);
+
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
   return (
     <>
       <h1>List</h1>
+      {items.length === 0 && <p> Não foi encontrado nenhum item</p>}
+
       <ul className="list-group">
-        <li className="list-group-item">Cras justo odio</li>
-        <li className="list-group-item">Dapibus ac facilisis in</li>
-        <li className="list-group-item">Morbi leo risus</li>
-        <li className="list-group-item">Porta ac consectetur ac</li>
-        <li className="list-group-item">Vestibulum at eros</li>
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active " // if true
+                : "list-group-item" //if false
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+          >
+            {item}
+          </li>
+        ))}
       </ul>
     </>
   );
