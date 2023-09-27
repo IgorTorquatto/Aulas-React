0dd9d372f7dd172647d90d559c390f191e588a88
igor <igor.torquato.20@gmail.com>
2023-09-27 14:33:10 -0300

    add react

---
+++ React(Crash_Course)/myapp/src/App.js
@@ -0,0 +1,12 @@
+import Componente1 from './components/Componente1';
+import 'bootstrap/dist/css/bootstrap.min.css'; //Usando bootstrap 
+
+function App() {
+  return (
+    <>
+    <Componente1/>
+    </>
+  );
+}
+
+export default App;
---
+++ React(Crash_Course)/myapp/src/components/Componente1.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Componente1() {
+  return (
+   <>
+   <button className="btn btn-primary">Clique em mim</button>
+   </>
+  )
+}
+
+export default Componente1
